18328f42d07a5035229fbd2bee8b578fec7e0b8a
anusri-v <anusri.v@shopup.org>
2026-03-20 00:50:51 +0530

    Matches controller: Add matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,27 @@
 class MatchesController < ApplicationController
+  def create
+    @player1 = Player.find_by(id: params[:player_1_id])
+    @player2 = Player.find_by(id: params[:player_2_id])
+
+    render json: { success: false, message: 'Player 1 is not valid' } and return if @player1.nil?
+
+    render json: { success: false, message: 'Player 2 is not valid' } and return if @player2.nil?
+
+    unless [params[:player_1_id], params[:player_2_id]].include?(params[:winner_id])
+      render json: { success: false, message: 'Winner should be either player 1 or player 2' } and return
+    end
+
+    @match = Match.new(match_params.merge(scheduled_on: Time.now))
+    if @match.save
+      render json: { success: true, message: 'Match created successfully', match: @match }
+    else
+      render json: { success: false, message: "Error in creating match: #{@match.errors.full_messages}", match: nil }
+    end
+  end
+
+  private
+
+  def match_params
+    params.permit(:player_1_id, :player_2_id, :winner_id)
+  end
 end
---
+++ spec/requests/matches/create_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Matches API', type: :request do
+  describe 'POST /matches' do
+    context 'Failure case' do
+      it 'Should fail when player 1 is invalid' do
+        player1 = create(:player, name: 'Player 1')
+        player2 = create(:player, name: 'Player 2')
+        post '/matches', params: { player_1_id: player1.id + 10_000, player_2_id: player2.id, winner_id: player1.id }
+
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to eq false
+        expect(json_response['message']).to include('Player 1 is not valid')
+        expect(json_response['match']).to be_nil
+      end
+
+      it 'Should fail when player 2 is invalid' do
+        player1 = create(:player, name: 'Player 1')
+        player2 = create(:player, name: 'Player 2')
+        post '/matches', params: { player_1_id: player1.id, player_2_id: player2.id + 10_000, winner_id: player1.id }
+
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to eq false
+        expect(json_response['message']).to include('Player 2 is not valid')
+        expect(json_response['match']).to be_nil
+      end
+
+      it 'Should fail when winner ID is invalid' do
+        player1 = create(:player, name: 'Player 1')
+        player2 = create(:player, name: 'Player 2')
+        post '/matches', params: { player_1_id: player1.id, player_2_id: player2.id, winner_id: player1.id - 1 }
+
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to eq false
+        expect(json_response['message']).to include('Winner should be either player 1 or player 2')
+        expect(json_response['match']).to be_nil
+      end
+    end
+
+    context 'Success case' do
+      it 'Should create a match record properly' do
+        player1 = create(:player, name: 'Player 1')
+        player2 = create(:player, name: 'Player 2')
+        post '/matches', params: { player_1_id: player1.id, player_2_id: player2.id, winner_id: player1.id }
+
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to eq true
+        expect(json_response['message']).to include('Match created successfully')
+        expect(json_response['match']).not_to be_nil
+        expect(json_response['match']['player_1_id']).to eq player1.id
+        expect(json_response['match']['player_2_id']).to eq player2.id
+        expect(json_response['match']['winner_id']).to eq player1.id
+      end
+    end
+  end
+end
